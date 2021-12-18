17fddbf2a97af20c1a92869a654548a44ec35023
ebachard <ebachard@users.noreply.github.com>
2021-12-18 11:16:21 +0100

    Simplify the build
    A bash script to allow an easy ImStudio build / installation

---
+++ create_build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Author : Eric Bachard
+# License MIT
+# Created : friday 17 déc. 2021 21:27:36 CET
+
+BUILD_DIR=build
+
+# other case is LEGACY if nothing works for you
+OPENGL_GL_PREFERENCE=GLVND
+
+S_UNAME=`uname -s`
+
+if test X${S_UNAME} = "XLinux"
+    then
+
+
+        if ! test -d ${BUILD_DIR}
+           then
+                mkdir ${BUILD_DIR}
+        fi
+
+        cd ${BUILD_DIR}
+
+        # push the current build directory
+        pushd .
+        # build
+        cmake -DOpenGL_GL_PREFERENCE=GLVND \
+        ..
+        make -j4
+
+        # install. Uncomment me if you have admin rights
+        # sudo make install
+        # sudo ldconfig
+        popd
+fi
+
+exit
+
+
